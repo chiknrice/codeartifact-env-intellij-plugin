575add60e7b741e724437ea9faa65c63f1fc17ed
GitHub Action <action@github.com>
2022-06-02 02:24:30 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "codeartifact-env-intellij-plugin"
---
+++ src/main/kotlin/com/github/chiknrice/codeartifactenvintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.chiknrice.codeartifactenvintellijplugin.services
+
+import com.github.chiknrice.codeartifactenvintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
